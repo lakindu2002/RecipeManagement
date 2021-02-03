7f9c5490b3d5824e7a2137b20ac067cc7216b0e2
lakindu2002 <lakindu2002@live.com>
2021-02-03 08:28:45 +0530

    Added a Preloading Strategy to the Lazy Loaded Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
 
 
@@ -25,7 +25,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
